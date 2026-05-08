926d6c72eb83e7dbd6033e1e00b6272a080681b9
fmf <929433643@qq.com>
2026-05-08 18:06:31 +0800

    monotone_stack add nextgreater prevgreater

--- monotone_stack.go
+++ monotone_stack.go
@@ -1,5 +1,7 @@
 package main
 
+import "math"
+
 func leftGreater(nums []int) []int {
 	n := len(nums)
 	left := make([]int, n)
@@ -50,14 +52,94 @@ func dailyTemperatures(temperatures []int) []int {
 func finalPrices(prices []int) []int {
 	n := len(prices)
 	ans := make([]int, n)
-	st := make([]int, 0) // 存储value就好了
-	// 递增栈(从栈底到栈顶)
-	for i := 0; i < n; i++ {
-		for len(st) > 0 && st[len(st)-1] >= prices[i] {
-			// pop up
+	st := []int{0} // 哨兵值
+	// 从右向左维持一个递增序列
+	// 进行一次循环的时候
+	// 找到右侧第一个小于当前值的价格
+	for i := n - 1; i >= 0; i-- {
+		for st[len(st)-1] > prices[i] {
 			st = st[:len(st)-1]
 		}
-		// ans[i] =
+		ans[i] = prices[i] - st[len(st)-1]
+		st = append(st, prices[i])
 	}
+	return ans
+}
+
+func nextGreaterElement(nums1 []int, nums2 []int) []int {
+	n, m := len(nums1), len(nums2)
+	idx := make(map[int]int, n)
+	for i, x := range nums1 {
+		idx[x] = i
+	}
+	ans := make([]int, n)
+	for i := range ans {
+		ans[i] = -1
+	}
+	st := []int{}
+	for i := m - 1; i >= 0; i-- {
+		x := nums2[i]
+		// 单调递减
+		for len(st) > 0 && st[len(st)-1] < x {
+			st = st[:len(st)-1]
+		}
+		if len(st) > 0 {
+			if j, ok := idx[x]; ok {
+				ans[j] = st[len(st)-1]
+			}
+		}
+		st = append(st, x)
+	}
+	return ans
+}
+
+func nextGreaterElements(nums []int) []int {
+	n := len(nums)
+	ans := make([]int, n)
+	for i := range n {
+		ans[i] = -1
+	}
+	st := []int{}
+	for i := 2*n - 1; i >= 0; i-- {
+		x := nums[i%n]
+		// 保持严格递减
+		for len(st) > 0 && st[len(st)-1] <= x {
+			st = st[:len(st)-1]
+		}
+		if len(st) > 0 {
+			ans[i%n] = st[len(st)-1]
+		}
+		st = append(st, x)
+	}
+	return ans
+}
+
+// 1,2,3,4,3,1,2,3,4,3
+// 1,2,1,1,2,1
+
+// 上一个大于当前元素的值的距离
+type pair struct {
+	price int
+	day   int
+}
+type StockSpanner struct {
+	st  []pair
+	cur int // cur day
+}
+
+func Constructor3() StockSpanner {
+	return StockSpanner{
+		st:  []pair{{price: math.MaxInt, day: -1}},
+		cur: -1,
+	}
+}
+
+func (ss *StockSpanner) Next(price int) int {
+	ss.cur++
+	for ss.st[len(ss.st)-1].price <= price {
+		ss.st = ss.st[:len(ss.st)-1]
+	}
+	ans := ss.cur - ss.st[len(ss.st)-1].day
+	ss.st = append(ss.st, pair{price: price, day: ss.cur})
 	return ans
 }
